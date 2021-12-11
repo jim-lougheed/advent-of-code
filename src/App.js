2f7ca89d9a68b8780569d41f5e75b467f4f52fef
Jim Lougheed <jims.codes@gmail.com>
2021-12-11 00:26:49 -0500

    Complete Day 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Day3 from './pages/Day3';
+import Day4 from './pages/Day4/Day4';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <h1>Advent of Code</h1>
             <Switch>
               <Route path='/day3' exact component={Day3}/>
+              <Route path='/day4' exact component={Day4}/>
             </Switch>
         </header>
       </div>
